1a875a3edb013ebb61b0fc4c31cd5905a5caa8ff
Mark Canlas <github@htmlism.com>
2018-09-01 16:35:31 -0400

    added scalashow

---
+++ src/main/scala/com/htmlism/shadows/plato/ScalaShow.scala
@@ -0,0 +1,42 @@
+package com.htmlism.shadows.plato
+
+import scalaz._, Scalaz._
+
+trait ScalaShow[A] {
+  def show(x: A): String
+}
+
+object ScalaShow {
+  implicit val ssDataClass: ScalaShow[DataClass] =
+    new ScalaShow[DataClass] {
+      def show(x: DataClass): String = {
+        val typeParameters = x.typeParameters.map(_.name).toList.mkString(", ")
+
+        val constructors =
+          x.constructors.map(cToStr(x)).toList
+
+        val groups =
+          List(s"sealed trait ${x.name}[$typeParameters]") ::: constructors
+
+        groups.mkString("\n\n")
+      }
+    }
+
+  private def cToStr(x: DataClass)(c: Constructor) = {
+    if (c.typeSignatures.isEmpty)
+      "case object " + c.name
+    else {
+      val parameters =
+        c.typeSignatures
+          .map {
+            case BasicType(s) => s
+            case ConstructedOne(f, a) => s"$f[$a]"
+            case _ => throw new IllegalStateException
+          }
+          .map(t => s"_: $t")
+          .mkString(", ")
+
+      "case class " + c.name + s"($parameters) extends ${x.name}" // TODO parameters
+    }
+  }
+}
