6c4d3baf56a565821b195c0e0a1f646a6bf2113f
Mark Canlas <github@htmlism.com>
2018-09-01 17:22:41 -0400

    move classes outside of the package object

--- src/main/scala/com/htmlism/shadows/plato/package.scala
+++ src/main/scala/com/htmlism/shadows/plato/package.scala
@@ -6,63 +6,18 @@ package object plato {
   type Nel[A] = NonEmptyList[A]
   val Nel = NonEmptyList
 
-  object TypeClass {
-    def k0(name: String, parameter: Star): OverStar =
-      OverStar(name, parameter, Nil)
-
-    def k1(name: String, parameter: StarStar): OverStarStar =
-      OverStarStar(name, parameter, Nil)
-  }
-
-  sealed trait TypeClass {
-    def name: String
-
-    def methods: List[Method]
-  }
-
-  case class OverStar(name: String, parameter: Star, methods: List[Method]) extends TypeClass {
-    def w(m: Method): OverStar = copy(methods = m +: methods)
-  }
-
-  case class OverStarStar(name: String, parameter: StarStar, methods: List[Method]) extends TypeClass {
-    def w(m: Method): OverStarStar = copy(methods = m +: methods)
-  }
-
-  sealed trait Kind {
-    def name: String
-  }
-
-  sealed trait Star extends Kind
-  sealed trait StarStar extends Kind
-
-  case class NullaryTypeConstructor(name: String) extends Star
-  case class UnaryTypeConstructor(name: String) extends StarStar
-
-  case class ConstrainedNtc(name: String, constraint: OverStar) extends Star
-  case class ConstrainedUtc(name: String, constraint: OverStarStar) extends StarStar
-
-  object Method {
-    def apply(name: String, signature: TypeSignature): Method =
-      Method(name, None, signature)
-  }
-  case class Method(name: String, symbolicAlias: Option[String], signature: TypeSignature)
-
-  sealed trait TypeSignature {
-    def =>:(left: TypeSignature): FunctionConsType =
-      FunctionConsType(left, this)
-  }
-
-  sealed trait TerminalTypeSignature extends TypeSignature
-
-  case class BasicType(name: String) extends TerminalTypeSignature
-  case class ConstructedOne(f: String, a: String) extends TerminalTypeSignature
-
-  case class FunctionConsType(a: TypeSignature, b: TypeSignature) extends TypeSignature
-
   val A = BasicType("A")
   val B = BasicType("B")
   val FA = ConstructedOne("F", "A")
   val FB = ConstructedOne("F", "B")
+
+  implicit class Ops(s: Symbol) {
+    def ntc: NullaryTypeConstructor =
+      NullaryTypeConstructor(s.name)
+
+    def utc: UnaryTypeConstructor =
+      UnaryTypeConstructor(s.name)
+  }
 
   def linearize(ts: TypeSignature): (List[TypeSignature], TerminalTypeSignature) = {
     @annotation.tailrec
@@ -76,13 +31,5 @@ package object plato {
       }
 
     rec(ts, Nil)
-  }
-
-  implicit class Ops(s: Symbol) {
-    def ntc: NullaryTypeConstructor =
-      NullaryTypeConstructor(s.name)
-
-    def utc: UnaryTypeConstructor =
-      UnaryTypeConstructor(s.name)
   }
 }
