a1d6abc2ae9fdadaac8d9e254e09a13a6e0cfec0
Mark Canlas <github@htmlism.com>
2018-09-03 20:04:41 -0400

    compile stubs for scala adt (#4)

--- src/main/scala/com/htmlism/shadows/ShadowShow.scala
+++ src/main/scala/com/htmlism/shadows/ShadowShow.scala
@@ -1,5 +1,13 @@
 package com.htmlism.shadows
 
+object ShadowShow {
+  implicit def shadowForList[A](implicit s: ShadowShow[A]): ShadowShow[List[A]] =
+    new ShadowShow[List[A]] {
+      def show(xs: List[A]): String =
+        xs.map(s.show).mkString("\n\n")
+    }
+}
+
 trait ShadowShow[A] {
   def show(x: A): String
 }
