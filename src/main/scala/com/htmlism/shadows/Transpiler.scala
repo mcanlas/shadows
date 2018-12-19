38f92d2861336ade0a353f908cbeb1f6b38eb967
Mark Canlas <github@htmlism.com>
2018-12-19 17:26:04 -0500

    add scala doc

--- src/main/scala/com/htmlism/shadows/Transpiler.scala
+++ src/main/scala/com/htmlism/shadows/Transpiler.scala
@@ -1,5 +1,11 @@
 package com.htmlism.shadows
 
+/**
+  * A contract for converting constructs of language `A` to language `B`.
+  *
+  * @tparam A The source language
+  * @tparam B The destination language
+  */
 trait Transpiler[A, B] {
   def transpile(a: A): B
 }
