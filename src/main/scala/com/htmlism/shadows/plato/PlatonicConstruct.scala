399edb3eb19b8e6e0ab8229d52f4e3359c4bb842
Mark Canlas <github@htmlism.com>
2019-02-28 19:29:43 -0500

    put type class in platonic hierarchy

--- src/main/scala/com/htmlism/shadows/plato/PlatonicConstruct.scala
+++ src/main/scala/com/htmlism/shadows/plato/PlatonicConstruct.scala
@@ -1,5 +1,8 @@
 package com.htmlism.shadows.plato
 
+/**
+  * A top-level Platonic construct is either a data class or a type class.
+  */
 sealed trait PlatonicConstruct
 
 object DataClass {
@@ -42,3 +45,33 @@ case class Constructor(name: String, parameters: List[Parameter])
   * @param name Not used in Haskell.
   */
 case class Parameter(name: String, sig: TypeSignature)
+
+// ====================================================================================================
+
+object TypeClass {
+  def k0(name: String, parameter: Star): OverStar =
+    OverStar(name, parameter, Nil)
+
+  def k1(name: String, parameter: StarStar): OverStarStar =
+    OverStarStar(name, parameter, Nil)
+}
+
+sealed trait TypeClass extends PlatonicConstruct {
+  def name: String
+
+  def methods: List[Method]
+}
+
+/**
+  * Think `Semigroup[A]`.
+  */
+case class OverStar(name: String, parameter: Star, methods: List[Method]) extends TypeClass {
+  def w(m: Method): OverStar = copy(methods = m +: methods)
+}
+
+/**
+  * Think `Monad[List]` where `List[A]`.
+  */
+case class OverStarStar(name: String, parameter: StarStar, methods: List[Method]) extends TypeClass {
+  def w(m: Method): OverStarStar = copy(methods = m +: methods)
+}
